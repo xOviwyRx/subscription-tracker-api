c6b484f5b5594317b5d90a3d0d2552c027a4d05d
Andrey Yurov <a.yurov6@gmail.com>
2025-07-27 04:48:45 +0500

    Add config file

---
+++ internal/config/config.go
@@ -0,0 +1,95 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server   ServerConfig   `yaml:"server"`
+	Database DatabaseConfig `yaml:"database"`
+}
+
+type ServerConfig struct {
+	Port string `yaml:"port"`
+	Host string `yaml:"host"`
+}
+
+type DatabaseConfig struct {
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	DBName   string `yaml:"dbname"`
+	SSLMode  string `yaml:"sslmode"`
+}
+
+func Load() (*Config, error) {
+	// Load .env file
+	if err := godotenv.Load(); err != nil {
+		// .env file is optional, so we don't return error
+		fmt.Println("No .env file found, using environment variables")
+	}
+
+	config := &Config{}
+
+	// Try to load from YAML file first
+	if yamlFile, err := os.ReadFile("config.yaml"); err == nil {
+		if err := yaml.Unmarshal(yamlFile, config); err != nil {
+			return nil, fmt.Errorf("error parsing config.yaml: %w", err)
+		}
+	}
+
+	// Override with environment variables if they exist
+	if port := os.Getenv("SERVER_PORT"); port != "" {
+		config.Server.Port = port
+	}
+	if host := os.Getenv("SERVER_HOST"); host != "" {
+		config.Server.Host = host
+	}
+	if dbHost := os.Getenv("DB_HOST"); dbHost != "" {
+		config.Database.Host = dbHost
+	}
+	if dbPort := os.Getenv("DB_PORT"); dbPort != "" {
+		config.Database.Port = dbPort
+	}
+	if dbUser := os.Getenv("DB_USER"); dbUser != "" {
+		config.Database.User = dbUser
+	}
+	if dbPassword := os.Getenv("DB_PASSWORD"); dbPassword != "" {
+		config.Database.Password = dbPassword
+	}
+	if dbName := os.Getenv("DB_NAME"); dbName != "" {
+		config.Database.DBName = dbName
+	}
+	if sslMode := os.Getenv("DB_SSLMODE"); sslMode != "" {
+		config.Database.SSLMode = sslMode
+	}
+
+	// Set defaults if not provided
+	if config.Server.Port == "" {
+		config.Server.Port = "8080"
+	}
+	if config.Server.Host == "" {
+		config.Server.Host = "localhost"
+	}
+	if config.Database.SSLMode == "" {
+		config.Database.SSLMode = "disable"
+	}
+
+	return config, nil
+}
+
+func (c *Config) GetDatabaseDSN() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		c.Database.Host,
+		c.Database.Port,
+		c.Database.User,
+		c.Database.Password,
+		c.Database.DBName,
+		c.Database.SSLMode,
+	)
+}
